2df2edd742f38d39c5839a1d69dd0db39542de22
Krang84 <patelassad@gmail.com>
2022-11-02 19:42:29 +0100

    Update scrit2.js

--- scrit2.js
+++ scrit2.js
@@ -1,1 +1,13 @@
-
+var word = "bottles";
+var count = 99;
+while(count>0) {
+  console.log(count +""+word+"of beer on the wall");
+  console.log(count +""+word+"of beer");
+  console.log("take one down");
+  count = count -1;
+  if(count>0) {
+    console.log(count +""+word+" of beer on the wall");
+  } else {
+    console.log("no more " + word + "of beer on the wall");
+  }
+}
