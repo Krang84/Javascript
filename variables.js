08e9eca5515e5f70a496a6abe2a5a5ed98062dd1
Krang84 <patelassad@gmail.com>
2023-01-04 23:07:11 +0100

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,19 @@
+// examples
+// 1.Variables with var
+
+// var variable_name = expression
+var favoriteFood = 'pizza';
+var numOfSlices = 8;
+
+// 2.variables with let
+
+// reassigned
+
+// let variable_name ; 
+
+// 3.constantes with const
+
+const entree ='Enchiladas'
+
+// TypeError : trying to reassign
+entree='Tacos'
