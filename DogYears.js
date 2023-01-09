901daba5acf8f18fc317922a16111f7e183b788e
Krang84 <patelassad@gmail.com>
2023-01-09 08:56:32 +0100

    Create DogYears.js

---
+++ DogYears.js
@@ -0,0 +1,17 @@
+const myAge = 8
+// my age at 2023
+let earlyYears = 2
+// new variable
+earlyYears *= 10.5
+let laterYears = myAge -2
+// an expression
+laterYears *= 4
+// * 4
+console.log(earlyYears)
+console.log(laterYears)
+let myAgeInDogYears = earlyYears + laterYears
+// addition
+let myName = "John".toLowerCase()
+// method call
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+// final result
